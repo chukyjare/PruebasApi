6b2bc4fc596193cb7d49748d953a5c54743cc302
chukyjare <chukyjare@gmail.com>
2022-05-20 18:03:45 +0200

    subida inicial
    subida inicial de api nasa

---
+++ ApiNasa/src/main/java/com/atmira/api/ApiNasaApplication.java
@@ -0,0 +1,13 @@
+package com.atmira.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiNasaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiNasaApplication.class, args);
+	}
+
+}
---
+++ ApiNasa/src/test/java/com/atmira/api/ApiNasaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atmira.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiNasaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
