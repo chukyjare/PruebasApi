dcae9e68dd2e4c1703f802c487edccd19c3f8de7
chukyjare <chukyjare@gmail.com>
2022-05-21 18:28:11 +0200

    implemento Exception de la api
    implemento modelo de gestion de errores de la apiRest

--- ApiNasa/src/main/java/com/atmira/api/exception/ForecasterException.java
+++ ApiNasa/src/main/java/com/atmira/api/exception/ForecasterException.java
@@ -1,5 +1,17 @@
 package com.atmira.api.exception;
 
-public class ForecasterException {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
+public class ForecasterException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2419626208122308372L;
+ 
+	private String code;
+	private String message;
+	private String description;
 }
